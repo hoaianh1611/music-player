f873fcd0110ecb23914b6c95df7c8d6c1968fea8
Sophie Nguyen <anh.nguyen.pd@gmail.com>
2022-05-23 11:23:32 +0700

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link, Typography } from "@mui/material";
+
+function Footer() {
+  return (
+    <Typography
+      variant="body2"
+      align="center"
+      p={1}
+      mt={2}
+      bottom="15px"
+      sx={{ color: "beige" }}
+    >
+      {"Copyright © "}
+      <Link color="inherit">Hoai Anh</Link>
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+export default Footer;
